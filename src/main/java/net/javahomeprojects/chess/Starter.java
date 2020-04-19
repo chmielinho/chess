5ffa877354f51b5ac9dd19289400bb224e84cd88
Mateusz <chmielinho@gmail.com>
2020-04-19 12:56:07 +0200

    Initial commit

---
+++ src/main/java/net/javahomeprojects/chess/Starter.java
@@ -0,0 +1,12 @@
+package net.javahomeprojects.chess;
+
+public class Starter {
+
+    public static void main(String[] args) {
+
+        String test = "Witaj, chyba działa";
+
+        System.out.println(test);
+    }
+
+}
